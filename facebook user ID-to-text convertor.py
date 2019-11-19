ab9707234cb310de171bcb7971bfc035b654f840
gujjs <56742286+gujjs@users.noreply.github.com>
2019-11-20 00:21:43 +0400

    Add files via upload

---
+++ facebook user ID-to-text convertor.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from selenium import webdriver
+from datetime import date
+import random
+import time
+
+#import os
+#pre = os.path.dirname(os.path.realpath(__file__))
+#fname = 'numbers.xlsx'
+#path = os.path.join(pre, fname)
+#Z = pd.read_excel(path)
+
+df = pd.read_excel('numbers.xlsx')
+target_list = df['working column'].tolist()
+
+driver = webdriver.Chrome('C:\\Users\Guja\Downloads\chromedriver')
+
+driver.get('https://www.facebook.com/php')
+email = 'guja1515@gmail.com'
+password = '*your password here*'
+
+driver.find_element_by_id('email').send_keys(email)
+driver.find_element_by_id('pass').send_keys(password)
+driver.find_element_by_id('loginbutton').click()
+
+z = 0
+#while z == 0:
+   #x = z + int(input())
+random = random.randint
+
+result_list = list()
+
+for x in target_list:
+    if str(x) == 'nan':
+        break
+    time.sleep(random(10,30))
+    target_url = str(x)
+    driver.get(target_url)
+    result_list.append(driver.current_url)
+    z = z + 1
+    print(z)
+    while z == 20:
+       print('break')
+       time.sleep(random(600,1200))
+       z = 0
+#print(result_list)
+
+export = pd.DataFrame(result_list, columns = ['Users'])
+#print(export)
+
+today = date.today()
+print(today)
+
+export.to_excel("new_acc's" + str(today) + ".xlsx")
+
+print('Job Done')
